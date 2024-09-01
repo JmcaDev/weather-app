44e796ac8f73329910083bdb68d709b978f4eb57
JmcaDev <jmcavzla@gmail.com>
2024-09-01 11:27:26 -0400

    Add fetch api

---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,25 @@
+
+import axios from "axios"
+import { SearchType } from "../types"
+
+function useWeather () {
+    const fetchWeather = async (search: SearchType) => {
+
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+            const {data} = await axios(geoUrl)
+            console.log(data)
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    } 
+
+    return {
+        fetchWeather
+    }
+}
+
+export default useWeather
